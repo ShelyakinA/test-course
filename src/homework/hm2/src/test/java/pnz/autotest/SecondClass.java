aec5e342d5db6f07ffce817a5943817c8023ec2d
ShelyakinA <zomgstyle@mail.ru>
2018-02-13 18:59:06 +0300

    homework2 added

---
+++ src/homework/hm2/src/test/java/pnz/autotest/SecondClass.java
@@ -0,0 +1,74 @@
+package pnz.autotest;
+
+import com.google.common.base.Verify;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class SecondClass {
+
+        private static WebDriver driver;
+
+
+        @BeforeClass
+        public static void setup(){
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\webdriver\\chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.get("https://yandex.ru/");
+
+        }
+
+
+        @Test
+        public void search(){
+
+            WebElement searchAndClick = driver.findElement(By.xpath("(//span[@class=\"link__inner\"])[1]"));
+            searchAndClick.click();
+            searchAndClick = driver.findElement(By.xpath("//a[@aria-label=\"Настройки портала\"]"));
+            searchAndClick.click();
+            searchAndClick= driver.findElement(By.xpath("//a[@aria-label=\"Язык\"]"));
+            searchAndClick.click();
+
+            WebElement findButton = driver.findElement(By.xpath("//*[@type=\"button\"]"));
+            findButton.click();
+            findButton.sendKeys(Keys.ARROW_DOWN);
+            findButton.sendKeys(Keys.ARROW_DOWN);
+            findButton.sendKeys(Keys.ARROW_DOWN);
+            findButton.sendKeys(Keys.ARROW_DOWN);
+            findButton.sendKeys(Keys.ARROW_DOWN);
+            findButton.sendKeys(Keys.ENTER);
+
+            WebElement saveButton = driver.findElement(By.xpath("//*[@type=\"submit\"]"));
+            saveButton.click();
+
+            searchAndClick = driver.findElement(By.xpath("(//span[@class=\"link__inner\"])[1]"));
+            searchAndClick.click();
+            searchAndClick = driver.findElement(By.xpath("//a[@aria-label=\"Настройки портала\"]"));
+            searchAndClick.click();
+            searchAndClick = driver.findElement(By.xpath("//a[@aria-label=\"Language\"]"));
+            searchAndClick.click();
+
+            WebElement resultSettings = driver.findElement(By.xpath("(//span[@class=\"button__text\"])[1]"));
+            String result = resultSettings.getText();
+            Assert.assertEquals("English", result);
+        }
+
+
+        @AfterClass
+        public static void qiut(){
+            driver.quit();
+        }
+
+}
